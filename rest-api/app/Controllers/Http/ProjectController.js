e7b3f8353d19179b76875e0607bd57a91dee5404
jefxroz <jefxroz@gmail.com>
2022-02-05 02:13:38 -0600

    Uso EXCEP, Project

---
+++ rest-api/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Project = use("App/Models/Project");
+const AutorizacionService = use("App/Services/AutorizacionService");
+
+class ProjectController {
+  //cada proyecto pertence a un USR, valida que USR este auth
+  async index({ auth }) {
+    const user = await auth.getUser();
+    return await user.projects().fetch();
+  }
+
+  async create({ auth, request }) {
+    const user = await auth.getUser(); //toma USR
+    const { nombre } = request.all(); //toma el nombre de todo lo que se esta enviando al SRV
+    const project = new Project();
+    //llenar el pro con los datos que nos estan dando
+    project.fill({
+      nombre,
+    });
+    await user.projects().save(project);
+    return project;
+  }
+
+  async destroy({ auth, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AutorizacionService.verificarPermiso(project, user);
+    await project.delete();
+    return project;
+  }
+
+  async update({ auth, params, request }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AutorizacionService.verificarPermiso(project, user);
+    project.merge(request.only("nombre"));
+    await project.save();
+    return project;
+  }
+}
+module.exports = ProjectController;
